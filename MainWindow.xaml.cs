bce8f64bd1ad0b7acea3caf3b56b6f3a71b7eba5
Skewnart <corentinz@outlook.fr>
2022-11-19 23:58:42 +0100

    Upload project for launchsound shortcuts (skewpad)

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace launchsound
+{
+    public partial class MainWindow : Window
+    {
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        private const int HOTKEY_ID = 8496;
+
+        private IntPtr _windowHandle;
+        private HwndSource _source;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            _windowHandle = new WindowInteropHelper(this).Handle;
+            _source = HwndSource.FromHwnd(_windowHandle);
+            _source.AddHook(HwndHook);
+
+            //RegisterHotKey(_windowHandle, HOTKEY_ID, 0x0000, 0x71); //F2
+            //RegisterHotKey(_windowHandle, HOTKEY_ID, 0x0000, 0x72); //F3
+            //RegisterHotKey(_windowHandle, HOTKEY_ID, 0x0000, 0x73); //F4
+
+            RegisterHotKey(_windowHandle, HOTKEY_ID, 0x0000, 0x7C); //F13
+            RegisterHotKey(_windowHandle, HOTKEY_ID, 0x0000, 0x7D); //F14
+            RegisterHotKey(_windowHandle, HOTKEY_ID, 0x0000, 0x7E); //F15
+            RegisterHotKey(_windowHandle, HOTKEY_ID, 0x0000, 0x7F); //F16
+            RegisterHotKey(_windowHandle, HOTKEY_ID, 0x0000, 0x80); //F17
+            RegisterHotKey(_windowHandle, HOTKEY_ID, 0x0000, 0x81); //F18
+            RegisterHotKey(_windowHandle, HOTKEY_ID, 0x0000, 0x82); //F18
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            int lintParam = (int)((int)lParam * Math.Pow(16, -4));
+
+            switch (msg)
+            {
+                case 0x0312:        //Match pour les hotkeys
+                    this.playsound(lintParam);
+                    handled = true;
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void playsound(int lintParam)
+        {
+            SoundPlayer player = new SoundPlayer(Sound.GetSoundPath(lintParam));
+            try
+            {
+                player.Load();
+                player.Play();
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                MessageBox.Show($"Le son {Sound.GetSoundName(lintParam)} n'existe pas");
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            UnregisterHotKey(_windowHandle, HOTKEY_ID);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //this.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ Sound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace launchsound
+{
+    public static class Sound
+    {
+        private static readonly string LOCATION = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "sounds");
+        private static readonly Dictionary<int, string> SOUNDS = new Dictionary<int, string>
+        {
+            {0x7C, "pas faux.wav" },
+            {0x7D, "respirer compote.wav" },
+            {0x7E, "merde.wav" },
+            {0x7F, "dire des trucs.wav" },
+            {0x80, "fort ce con.wav" },
+            {0x81, "cuillere.wav" },
+            {0x82, "vous etes doué.wav" },
+        };
+
+        public static string GetSoundPath(int lintParam)
+        {
+            if (SOUNDS.ContainsKey(lintParam))
+                return Path.Combine(LOCATION, SOUNDS[lintParam]);
+            else
+                return null;
+        }
+
+        public static string GetSoundName(int lintParam)
+        {
+            if (SOUNDS.ContainsKey(lintParam))
+                return SOUNDS[lintParam];
+            else
+                return null;
+        }
+    }
+}
